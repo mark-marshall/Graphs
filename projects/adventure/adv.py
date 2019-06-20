612c204587999758b95afc5c6e4a0347998e341c
mark-marshall <mark.marshallgp@gmail.com>
2019-06-20 14:28:03 +0100

    Fixed comments to highlight random traversal rather than depth-first

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -41,7 +41,7 @@ for exit in exits:
 # add exits as edges with ?
 g.add_edge(0, list_exits)
 
-# depth-first traversal loop
+# random traversal loop
 while len(roomGraph):
     # hold the current room in a from_room 
     from_room = player.currentRoom.id
